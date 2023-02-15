5b0e9d57d9e34bce1b67e7ceeb289b0aa968d6b0
Potter J. Edgerton <106568662+potteredgerton@users.noreply.github.com>
2023-02-15 13:19:36 -0500

    Performance optimizations
    Moved all 4 calculate functions into 1 function named 'calculate' to save ram. Also placed most of the code (besides the calculate function) inside of a while True (infinite) loop so people do not need to restart the program after each calculation.
    
    Signed-off-by: Potter J. Edgerton <106568662+potteredgerton@users.noreply.github.com>

--- calc.py
+++ calc.py
@@ -1,26 +1,15 @@
-def add(num1, num2):
-    return num1 + num2
+def calculate(operator, num1, num2):
+    if (operator == +):
+        return num1 + num2
+    if (operator == -):
+        return num1 - num2
+    if (operator == *):
+        return num1 * num2
+    if (operator == /)
+        return num1 / num2
     
-def subtract(num1, num2):
-    return num2 - num1
-
-def multiply(num1, num2):
-    return num1 * num2
-    
-def divide(num1, num2):
-    return int(num1 / num2)
-    
-do = input("What operation would you like to do?\nA = Add 2 numbers\nS = Subtract 2 numbers\nM = Multiply 2 numbers\nD = Divide 2 numbers\n")
-
-n1 = int(input("Number 1:\n"))
-n2 = int(input("Number 2:\n"))
-
-if (do == "a" or do == "A"):
-    add(n1, n2)
-elif (do == "s" or do == "S"):
-    subtract(n1, n2)
-elif (do == "m" or do == "M"):
-    multiply(n1, n2)
-elif (do == "d" or do == "D"):
-    divide(n1, n2)
-
+while True:
+    operator = input("What operation would you like to do?\nA = Add 2 numbers\nS = Subtract 2 numbers\nM = Multiply 2 numbers\nD = Divide 2 numbers\n")
+    n1 = int(input("Number 1:\n"))
+    n2 = int(input("Number 2:\n"))
+    print(calculate(operator, n1, n2))
