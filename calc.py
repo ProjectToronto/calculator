c1e9f11554d95046cf8dc529f89657ed21b42f4a
Potter J. Edgerton <106568662+potteredgerton@users.noreply.github.com>
2023-02-16 12:19:27 -0500

    Fix syntax error
    Fixed a syntax error where I forgor to put the operators in double-quotes.
    
    Signed-off-by: Potter J. Edgerton <106568662+potteredgerton@users.noreply.github.com>

--- calc.py
+++ calc.py
@@ -1,11 +1,11 @@
 def calculate(operator, num1, num2):
-    if (operator == +):
+    if (operator == "+"):
         return num1 + num2
-    if (operator == -):
+    if (operator == "-"):
         return num1 - num2
-    if (operator == *):
+    if (operator == "*"):
         return num1 * num2
-    if (operator == /)
+    if (operator == "/")
         return num1 / num2
     
 while True:
